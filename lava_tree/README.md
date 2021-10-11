dbb0a0ad33fefccbbcddb94bb4e2045cf4420d37
Nicolas Ayala <nicoatnico.com@gmail.com>
2021-10-10 22:47:23 -0700

    lava tree scaffolding

---
+++ lava_tree/README.md
@@ -0,0 +1,22 @@
+---
+sort: 7
+---
+
+
+# Lava lamp phylogeny creator
+
+
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<script src="quizlogic.js"></script>
+
+<form id="quiz" onsubmit="HandleQuiz(this); return false">
+</form>
+<form id="results"></form>
+
+</body>
+</html> 
+
---
+++ lava_tree/tree.js
@@ -0,0 +1,77 @@
+var canvas = document.getElementById("myCanvas");
+canvas.addEventListener("mousedown", doMouseDown, false);
+var ctx = canvas.getContext("2d");
+
+canvas.width = canvas.getBoundingClientRect().width
+canvas.height = canvas.getBoundingClientRect().height
+var width = canvas.width;
+var height = canvas.height;
+
+var mouseX = 0;
+var mouseY = 0;
+
+var last_frame = Date.now()
+var this_frame = Date.now()
+
+function init() {
+    last_frame = Date.now()
+    this_frame = Date.now()
+    window.requestAnimationFrame(draw);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function init() {
+    window.requestAnimationFrame(draw);
+}
+
+
+
+
+
+
+
+
+
+
+var seconds = 0;
+
+function draw() {
+    canvas.width = canvas.getBoundingClientRect().width
+    canvas.height = canvas.getBoundingClientRect().height
+    width = canvas.width;
+    height = canvas.height;
+
+    last_frame = this_frame
+    this_frame = Date.now()
+    var sec = (this_frame - last_frame) / 1000.0
+    seconds += sec;
+
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(0, 0, width, height);
+
+
+    ctx.fillStyle = "#000000"
+    ctx.fillRect(10 + 10*seconds, 10, 10, 10)
+
+
+    window.requestAnimationFrame(draw);
+}
+
+
+
+function doMouseDown(e){
+
+    if(e.offsetX) {
+        mouseX = e.offsetX;
+        mouseY = e.offsetY;
+    }
+    else if(e.layerX) {
+        mouseX = e.layerX;
+        mouseY = e.layerY;
+    }
+}
+
+init();
